34b623a6a095c1d7c445e3df044884ee1b314045
Gerffson <gpjunior@uolinc.com>
2020-08-21 12:04:30 -0300

    Primeiro teste para exibir Saldo

---
+++ demo/src/main/java/com/example/demo/service/CaixaService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+public class CaixaService {
+
+    public Double getBalance() {
+        return 10.0;
+    }
+}
--- demo/src/test/java/com/example/demo/service/CaixaEletronicoTest.java
+++ demo/src/test/java/com/example/demo/service/CaixaEletronicoTest.java
@@ -1,5 +1,11 @@
 package com.example.demo.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.sun.media.sound.SoftMainMixer;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.AssertionErrors;
+
 /**
  * Exibir saldo ("Saldo: X")
  * Sacar um valor com sucesso, exibir mensagem e saldo resultante ("Saque no valor de: X. Saldo: Y")
@@ -9,6 +15,14 @@ package com.example.demo.service;
  * Sacar de uma conta e depositar em outra ("Movimentação realizada com sucesso")
  */
 public class CaixaEletronicoTest {
+
+
+    @Test
+    public void shouldShowBalance(){
+        CaixaService caixaService = new CaixaService();
+        Double balance = caixaService.getBalance();
+        assertEquals(10.0, balance);
+    }
 
 
 }
