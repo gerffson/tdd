d1acc16ff36a0bcf4e3a4376a04433ea0114f374
Gerffson <gpjunior@uolinc.com>
2020-08-21 11:44:05 -0300

    Primeiro commit

---
+++ demo/src/main/java/com/example/demo/entity/Conta.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Conta {
+
+    private int numero;
+    private double saldo;
+
+}
---
+++ demo/src/test/java/com/example/demo/service/CaixaEletronicoTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+/**
+ * Exibir saldo ("Saldo: X")
+ * Sacar um valor com sucesso, exibir mensagem e saldo resultante ("Saque no valor de: X. Saldo: Y")
+ * Sacar acima do limite (limite=1000) ("Não foi possível sacar.")
+ * Sacar saldo insuficiente ("Saldo insuficiente.")
+ * Depositar ("Deposito no valor de: X. Saldo: Y")
+ * Sacar de uma conta e depositar em outra ("Movimentação realizada com sucesso")
+ */
+public class CaixaEletronicoTest {
+
+
+}
